f5e0412cc8693000054f2374066d890398ea9437
Héloise Akohou <ak.heloise2007@gmail.com>
2025-10-08 16:42:29 +0100

    Correction de la fonction modePreference et du fichier mode.json

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async () =>{
 try {
      let response = await fetch (`${config.API_URL}/mode`); 
      let data = await response.json();
-     let theme = data.mode;
+     let theme = data[0].mode;
         
         // CORRECTION CRITIQUE : Utiliser '===' pour la comparaison, pas '=' pour l'affectation
     if (theme === 'dark-mode') { 
